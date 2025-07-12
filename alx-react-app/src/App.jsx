b3236c86ed2cb48a3e7f5c73f20bf0a3f304eea6
Brian Letting <brianletting46@gmail.com>
2025-07-12 21:00:42 +0300

    Added Userprofile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,25 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; // 👈 New import
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+
+      <UserProfile
+        name= "Brian"
+        age="28"
+        bio="Future full-stack dev & frontend master"
+        />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+
       <Footer />
     </>
   );
