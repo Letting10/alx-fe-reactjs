4427110e8a97510081cfc603ebd28dfed29ad682
Brian Letting <brianletting46@gmail.com>
2025-07-20 14:33:48 +0300

    react styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e6f0ff', color: '#333', fontFamily: 'Arial' }}>
+      <p>Welcome to the list of my favorite cities around the world!</p>
+    </main>
+  );
+}
+
+export default MainContent;
