7af61ba648211eb1c0dc3cbb38373ef4c35e7524
Brian Letting <brianletting46@gmail.com>
2025-08-07 11:13:27 +0300

    modified App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,22 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import EditRecipeForm from './components/EditRecipeForm';
+import RecipeDetail from './components/RecipeDetails';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div style={{ padding: '1rem' }}>
-        <h1>Recipe Sharing App</h1>
-        <SearchBar />
-        <RecipeList />
-      </div>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/edit/:id" element={<EditRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
