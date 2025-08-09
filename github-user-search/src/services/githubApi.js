89e3bd0f5fdbb42ab8439df87b9220072c90b6f8
Brian Letting <brianletting46@gmail.com>
2025-08-09 15:19:03 +0300

    Add github-user-search

---
+++ github-user-search/src/services/githubApi.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+export const searchGitHubUsers = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`, {
+    headers: GITHUB_API_KEY ? { Authorization: `token ${GITHUB_API_KEY}` } : {}
+  });
+  return response.data;
+};
